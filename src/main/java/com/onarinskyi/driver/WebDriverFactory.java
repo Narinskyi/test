c65c163fe2bc43702909a09296289b9aed16a27d
oleksiinarinskyi <oleksii.narinskyi@gmail.com>
2018-01-12 10:40:15 +0100

    Refactor WebDriverFactory - add configurable device name, update deprecated constructors

--- src/main/java/com/onarinskyi/driver/WebDriverFactory.java
+++ src/main/java/com/onarinskyi/driver/WebDriverFactory.java
@@ -5,9 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -15,18 +18,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@PropertySource("driver.properties")
 public class WebDriverFactory {
+
+    @Value("device.name")
+    private String deviceName;
 
     public WebDriver getDriverOf(BrowserType browserType) {
         switch (browserType) {
             case CHROME:
                 return new ChromeDriver();
             case FIREFOX:
-                return new FirefoxDriver(getFirefoxCapabilities());
+                return new FirefoxDriver(getFirefoxOptions());
             case MOBILE_EMULATOR_CHROME:
-                return new ChromeDriver(getChromeMobileCapabilities("Google Nexus 5"));
+                return new ChromeDriver(getChromeMobileOptions(deviceName));
             case TABLET_EMULATOR_CHROME:
-                return new ChromeDriver(getChromeTabletCapabilities());
+                return new ChromeDriver(getChromeTabletOptions());
             default:
                 return new ChromeDriver();
         }
@@ -37,17 +44,17 @@ public class WebDriverFactory {
             case CHROME:
                 return new RemoteWebDriver(hubHost, DesiredCapabilities.chrome());
             case FIREFOX:
-                return new RemoteWebDriver(hubHost, getFirefoxCapabilities());
+                return new RemoteWebDriver(hubHost, getFirefoxOptions());
             case MOBILE_EMULATOR_CHROME:
-                return new RemoteWebDriver(hubHost, getChromeMobileCapabilities("Google Nexus 5"));
+                return new RemoteWebDriver(hubHost, getChromeMobileOptions(deviceName));
             case TABLET_EMULATOR_CHROME:
-                return new RemoteWebDriver(hubHost, getChromeTabletCapabilities());
+                return new RemoteWebDriver(hubHost, getChromeTabletOptions());
             default:
                 return new RemoteWebDriver(hubHost, DesiredCapabilities.chrome());
         }
     }
 
-    private DesiredCapabilities getChromeTabletCapabilities() {
+    private ChromeOptions getChromeTabletOptions() {
         String userAgent = "Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 " +
                 "(KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10";
         ChromeOptions options = new ChromeOptions();
@@ -56,10 +63,10 @@ public class WebDriverFactory {
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
-        return capabilities;
+        return new ChromeOptions().merge(capabilities);
     }
 
-    private DesiredCapabilities getChromeMobileCapabilities(String deviceName) {
+    private ChromeOptions getChromeMobileOptions(String deviceName) {
         Map<String, Object> mobileEmulation = new HashMap<>();
         mobileEmulation.put("deviceName", deviceName);
 
@@ -68,13 +75,13 @@ public class WebDriverFactory {
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 
-        return capabilities;
+        return new ChromeOptions().merge(capabilities);
     }
 
-    private DesiredCapabilities getFirefoxCapabilities() {
+    private FirefoxOptions getFirefoxOptions() {
         DesiredCapabilities desiredCapabilities = DesiredCapabilities.firefox();
         FirefoxProfile profile = new FirefoxProfile();
         desiredCapabilities.setCapability(FirefoxDriver.PROFILE, profile);
-        return desiredCapabilities;
+        return new FirefoxOptions().merge(desiredCapabilities);
     }
 }
