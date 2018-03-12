4c0c36576330ab0eeb717c7e7ebf940191ceba67
oleksiinarinskyi <oleksii.narinskyi@gmail.com>
2018-03-12 22:17:41 +0100

    Add listeners

--- src/main/java/com/onarinskyi/core/AbstractTestNGTest.java
+++ src/main/java/com/onarinskyi/core/AbstractTestNGTest.java
@@ -5,6 +5,9 @@ import com.onarinskyi.driver.DriverManager;
 import com.onarinskyi.driver.WebDriverDecorator;
 import com.onarinskyi.listeners.TestNGExecutionListener;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Listeners;
@@ -13,6 +16,8 @@ import org.testng.asserts.SoftAssert;
 import javax.annotation.PostConstruct;
 
 @Listeners(TestNGExecutionListener.class)
+@TestExecutionListeners(inheritListeners = false, listeners =
+        {DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class})
 @ContextConfiguration(classes = AppConfig.class)
 public abstract class AbstractTestNGTest extends AbstractTestNGSpringContextTests {
 
